49f1645962e52e81a108810633d6c29ae14da1d0
Nicolas Morey-Chaisemartin <nmoreychaisemartin@suse.com>
2018-07-13 14:28:18 +0200

    release: fix release date handling
    Signed-off-by: Nicolas Morey-Chaisemartin <nmoreychaisemartin@suse.com>

--- release.sh
+++ release.sh
@@ -5,8 +5,22 @@ MAJOR=$(echo $VER | sed -e 's/\.[0-9]*$//')
 MINOR=$(echo $VER | sed -e 's/.*\.\([0-9]*\)$/\1/')
 N_MINOR=$(expr $MINOR + 1)
 N_VER="$MAJOR.$N_MINOR"
+# Update release date
+sed -i -e "s/$VER *$/$VER $(date '+ (%Y-%m-%d)')/" CHANGELOG
+
 CHANGES=$(cat CHANGELOG |
 			  awk ' BEGIN {count=0} {if ($1 == "------------------") count++; if (count <= 2) print $0}')
+
+mv CHANGELOG CHANGELOG.old
+cat <<EOF > CHANGELOG
+------------------
+$N_VER
+------------------
+
+EOF
+cat CHANGELOG.old >> CHANGELOG
+rm CHANGELOG.old
+
 tag_file=$(mktemp)
 cat <<EOF  > $tag_file
 git-maintain $VER
@@ -14,16 +28,7 @@ git-maintain $VER
 Changelog:
 $CHANGES
 EOF
-git tag -a -s v$VER --edit -F $tag_file && (
-	mv CHANGELOG CHANGELOG.old
-	cat <<EOF > CHANGELOG
-------------------
-$N_VER
-------------------
 
-EOF
-	cat CHANGELOG.old >> CHANGELOG
-	rm CHANGELOG.old
-	git commit CHANGELOG -m "Bump changelog version to $N_VER"
-)
+git commit CHANGELOG -F $tag_file --edit &&
+	git tag -a -s v$VER --edit -F $tag_file
 rm -f $tag_file
