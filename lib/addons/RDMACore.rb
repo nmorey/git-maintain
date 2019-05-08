484952c022eec869e47211f074f0f98228b0d187
Nicolas Morey-Chaisemartin <nmoreychaisemartin@suse.com>
2019-05-08 09:16:19 +0200

    RDMACore: Support major releases
    Signed-off-by: Nicolas Morey-Chaisemartin <nmoreychaisemartin@suse.com>

--- lib/addons/RDMACore.rb
+++ lib/addons/RDMACore.rb
@@ -7,9 +7,28 @@ module GitMaintain
                          chomp().gsub(/[sS][eE][tT]\(PACKAGE_VERSION\s*"([0-9.]*)".*$/, '\1')
             ver_nums = prev_ver.split(".")
             new_ver =  (ver_nums[0 .. -2] + [ver_nums[-1].to_i() + 1 ]).join(".")
-            git_prev_ver = "v" + (ver_nums[-1] == "0" ? ver_nums[0 .. -2].join(".") : prev_ver)
+            rel_ver = new_ver
+            rel_type = :stable
+            commit_msg = "Bump to version"
 
-            puts "Preparing release #{prev_ver} => #{new_ver}"
+            if @branch_type == :user_specified && @local_branch == "master"
+                rel_type = :major
+                new_ver = ([ ver_nums[0].to_i() + 1] + ver_nums[1 .. -1]).join(".")
+                rel_ver = prev_ver
+                prev_ver = ([ ver_nums[0].to_i() - 1] + ver_nums[1 .. -1]).join(".")
+                ver_nums = prev_ver.split(".")
+                commit_msg ="Update library version to be"
+            end
+
+            git_prev_ver = "v" + prev_ver
+            # Older tags might do have the terminal minor version (.0) for major releases
+            @repo.runGit("rev-parse --verify --quiet #{git_prev_ver}")
+            if $? != 0 then
+                # Try without the minor version number
+                git_prev_ver = "v" + ver_nums[0 .. -2].join(".")
+            end
+
+            puts "Preparing #{rel_type.to_s} release #{prev_ver} => #{rel_ver}"
             rep = GitMaintain::checkLog(opts, @local_branch, git_prev_ver, "release")
             if rep != "y" then
                 puts "Skipping release"
@@ -20,15 +39,25 @@ module GitMaintain
             tag_path=`mktemp`.chomp()
             puts tag_path
             tag_file = File.open(tag_path, "w+")
-            tag_file.puts "rdma-core-#{new_ver}:"
+            tag_file.puts "rdma-core-#{rel_ver}:"
             tag_file.puts ""
             tag_file.puts "Updates from version #{prev_ver}"
-            tag_file.puts " * Backport fixes:"
-            tag_file.puts `git log HEAD ^#{git_prev_ver} --format='   * %s'`
+            if rel_type == :stable then
+                tag_file.puts " * Backport fixes:"
+            end
+            tag_file.puts `git log HEAD ^#{git_prev_ver} --no-merges --format='   * %s'`
             tag_file.close()
 
+            if rel_type == :major
+                # For major, tag the current version first
+                @repo.runGit("tag -a -s v#{rel_ver} --edit -F #{tag_path}")
+                if $? != 0 then
+                    raise("Failed to tag branch #{local_branch}")
+                end
+            end
+
             # Update version number in relevant files
-            @repo.run("sed -i -e 's/\\(Version:[[:space:]]*\\)[0-9.]*/\\1#{new_ver}/g' redhat/rdma-core.spec suse/rdma-core.spec")
+            @repo.run("sed -i -e 's/\\(Version:[[:space:]]*\\)[0-9.]\\+/\\1#{new_ver}/g' redhat/rdma-core.spec suse/rdma-core.spec")
             @repo.run("sed -i -e 's/\\([sS][eE][tT](PACKAGE_VERSION[[:space:]]*\"\\)[0-9.]*\"/\\1#{new_ver}\"/g' CMakeLists.txt")
 
             @repo.run("cat <<EOF > debian/changelog.new
@@ -41,16 +70,18 @@ rdma-core (#{new_ver}-1) unstable; urgency=low
 $(cat debian/changelog)
 EOF
 mv debian/changelog.new debian/changelog")
-
             # Add and commit
             @repo.runGit("add  redhat/rdma-core.spec suse/rdma-core.spec CMakeLists.txt debian/changelog")
-            @repo.runGit("commit -m 'Bump to version #{new_ver}' --verbose --edit --signoff")
+            @repo.runGit("commit -m '#{commit_msg} #{new_ver}' --verbose --edit --signoff")
             if $? != 0 then
                 raise("Failed to commit on branch #{local_branch}")
             end
-            @repo.runGit("tag -a -s v#{new_ver} --edit -F #{tag_path}")
-            if $? != 0 then
-                raise("Failed to tag branch #{local_branch}")
+
+            if rel_type == :stable
+                @repo.runGit("tag -a -s v#{rel_ver} --edit -F #{tag_path}")
+                if $? != 0 then
+                    raise("Failed to tag branch #{local_branch}")
+                end
             end
             `rm -f #{tag_path}`
         end
