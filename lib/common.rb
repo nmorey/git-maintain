816170ca2ce25f379e940a185fa7a7b2e51bfb26
Nicolas Morey <nmorey@suse.com>
2026-06-01 09:34:52 +0200

    move string extension to its own file
    Signed-off-by: Nicolas Morey <nmorey@suse.com>

--- lib/common.rb
+++ lib/common.rb
@@ -1,5 +1,6 @@
 $LOAD_PATH.push(BACKPORT_LIB_DIR)
 
+require 'string'
 require 'ci'
 require 'travis'
 require 'azure'
@@ -7,39 +8,6 @@ require 'repo'
 require 'branch'
 
 $LOAD_PATH.pop()
-
-class String
-    # colorization
-    @@is_a_tty = nil
-    def colorize(color_code)
-        @@is_a_tty = STDOUT.isatty() if @@is_a_tty == nil
-        if @@is_a_tty then
-            return "\e[#{color_code}m#{self}\e[0m"
-        else
-            return self
-        end
-    end
-
-    def red
-        colorize(31)
-    end
-
-    def green
-        colorize(32)
-    end
-
-    def brown
-        colorize(33)
-    end
-
-    def blue
-        colorize(34)
-    end
-
-    def magenta
-        colorize(35)
-    end
-end
 
 module GitMaintain
     class Common
